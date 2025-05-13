89cfd304e0cdc14110faf9312ae0dc045bbee762
1m4nim <fancustard9@gmail.com>
2025-05-13 18:44:29 +0900

    HTMLで出力できた

---
+++ generatePDF.js
@@ -0,0 +1,52 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var PDFDocument = require("pdfkit");
+// JSONファイルを読み込む
+var data = JSON.parse(fs.readFileSync("output.json", "utf-8"));
+// PDF生成関数
+var createPDF = function (data, filename) {
+    if (filename === void 0) { filename = "output.pdf"; }
+    var doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream(filename));
+    var margin = 30;
+    var lineHeight = 15;
+    var top = 50;
+    var yPosition = top;
+    // ヘッダー
+    doc.fontSize(18).text("Hypomyces Fungi Data", margin, yPosition);
+    yPosition += lineHeight;
+    // テーブルのカラム名
+    doc
+        .fontSize(10)
+        .text("Hypomyces Species", margin, yPosition, { width: 120 })
+        .text("Host Genus", margin + 130, yPosition, { width: 120 })
+        .text("Host Species", margin + 260, yPosition, { width: 120 })
+        .text("Distribution", margin + 390, yPosition, { width: 120 })
+        .text("Source", margin + 510, yPosition, { width: 120 })
+        .text("Notes", margin + 630, yPosition, { width: 120 });
+    yPosition += lineHeight;
+    // 横線
+    doc.moveTo(margin, yPosition).lineTo(700, yPosition).stroke();
+    yPosition += 5;
+    // 各データ行を描画
+    data.forEach(function (entry) {
+        doc
+            .fontSize(10)
+            .text(entry.hypomyces_species, margin, yPosition, { width: 120 })
+            .text(entry.host_genus, margin + 130, yPosition, { width: 120 })
+            .text(entry.host_species, margin + 260, yPosition, { width: 120 })
+            .text(entry.distribution, margin + 390, yPosition, { width: 120 })
+            .text(entry.source, margin + 510, yPosition, { width: 120 })
+            .text(entry.notes, margin + 630, yPosition, { width: 120 });
+        yPosition += lineHeight;
+        // ページがいっぱいになったら新しいページを作成
+        if (yPosition > 750) {
+            doc.addPage();
+            yPosition = top;
+        }
+    });
+    doc.end();
+};
+// PDFを生成
+createPDF(data);
